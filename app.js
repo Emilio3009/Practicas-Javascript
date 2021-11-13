7b2d873d3c694f107fa45381ee731be2b8bbc246
Emilio Arreaza Sanchez <emilioarreazasanchez@MacBook-Pro-de-Emilio.local>
2021-11-13 20:00:32 +0100

    inicio de mi primer repo

---
+++ app.js
@@ -0,0 +1,12 @@
+let nombre = 'Emilio';
+let semaforo = 'gris';
+
+if(semaforo == 'rojo'){
+    console.log(`${nombre}, no puedes pasar`)
+} else if (semaforo == 'ambar'){
+    console.log(`${nombre}, Cuidado ten precaucion`)
+} else if (semaforo == 'verde'){
+    console.log (`${nombre}, Adelante, puede pasar`)
+} else console.log (`${nombre},Ese color, no es un color del semaforo`);
+
+typeof(nombre);
